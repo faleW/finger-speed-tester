35330d0ebbe6441f93ca657dcb3b7bf0cf62fe0d
wwytang2 <wwytang2-c@my.cityu.edu.hk>
2025-06-09 09:35:00 +0900

    dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import DarkModeToggle from '$lib/components/DarkModeToggle.svelte';
 	import TestProfileMenu from '$lib/components/TestProfileMenu.svelte';
 	import { Separator } from '$lib/components/ui/separator';
 	import '../app.css';
 	let { children } = $props();
+	import { ModeWatcher } from 'mode-watcher';
 </script>
+
 {#snippet SideBar()}
-    <div class="w-96 m-4">
-        Default
-        <Separator />
-        +Add
-    </div>
+	<div class="m-4 w-64">
+		
+		Welcome
+		<Separator />
+		Default
+		<Separator />
+		+Add
+
+		<Separator />
+		<DarkModeToggle />
+	</div>
 {/snippet}
 <div class="flex h-screen min-h-screen w-full flex-row overflow-hidden">
-    {@render SideBar()}
-    <Separator orientation="vertical" class="h" />
+	<ModeWatcher />
+	{@render SideBar()}
+	<Separator orientation="vertical" class="h" />
 	<main
-		class="flex h-screen min-h-screen w-full flex-col items-center justify-center p-4 select-none overflow-hidden"
+		class="flex h-screen min-h-screen w-full flex-col items-center justify-center overflow-hidden p-4 select-none"
 	>
 		<!-- <TestProfileMenu /> -->
 		{@render children?.()}

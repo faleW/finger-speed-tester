8b7875715f934ed28462f58000b0484b260dc421
wwytang2 <wwytang2-c@my.cityu.edu.hk>
2025-06-12 19:09:08 +0900

    Add bpm color

--- src/lib/constants/Color.ts
+++ src/lib/constants/Color.ts
@@ -1,6 +1,14 @@
 
 
-export const BPMColorSchema = ['#4395ff', '#66ff92', '#f8e85d', '#ff7f68', '#fe3971', '#6662dd', '#1f1f46']
+export const BPMColorSchema = [
+    '#4395ff',
+    '#66ff92', 
+    '#f8e85d', 
+    '#ff7f68', 
+    '#fe3971', 
+    '#6662dd', 
+    '#4548a4'
+];
 
 
 export type ThemeMode = 'light' | 'dark' | undefined;
